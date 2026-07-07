a5d2babae8f7cc2d654a1c23faf2e3cf7197db8a
cfranco1000 <cristianofrancosilva@gmail.com>
2026-07-07 14:42:16 -0300

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
+  adapter: vercel(),
   site: "https://tangram-book.vercel.app",
   vite: {
     plugins: [tailwindcss()],
